bfa9ed3b25a6d00cef51e7c388dc38633a3e2b11
codemaster <codemaster.han1022@gmail.com>
2025-04-22 13:33:37 -0700

    title fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { HeartRateMonitorDashboard } from "@/components/dashboard/heart-rate-mon
 export default function Home() {
   return (
     <main className="min-h-screen p-4 bg-background">
+      <h1> Real-time Chart</h1>
       <HeartRateMonitorDashboard />
     </main>
   )
